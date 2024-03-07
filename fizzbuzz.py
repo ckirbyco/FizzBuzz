1a424ac124c6a8da0291502a1c64b50630fb8f6e
ckirbyco <cek5dx@missouri.edu>
2024-03-06 21:28:09 -0600

    Committing changes made to step-two

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,9 @@
 # Fizzbuzz code
+# Branch step-two: Improving efficiency of the code
 
 # "Write a program that prints the numbers from 1 to 100.
-# iterate through the number 1-100 
-for x in range(101):
+# iterate through the number 1-100
+for x in range(1, 101):
     #print(x)
     # For numbers which are multiples of both three and five print “FizzBuzz”.
     if x%3 == 0 and x%5 == 0:
@@ -12,6 +13,10 @@ for x in range(101):
         print("Fizz")
     elif x%5 == 0:
         print("Buzz")
+    # Adding in else for when the conditions above aren't met. 
+    else:
+        print(x)
 
-
-
+# Testing range
+for y in range (1,101):
+    print(y)

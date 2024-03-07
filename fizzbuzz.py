039f8c7b91b74274bf7f53471304d3abb3851045
ckirbyco <cek5dx@missouri.edu>
2024-03-06 21:05:49 -0600

    Completing first step of FizzBuzz code. Adding to python application

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,17 @@
 # Fizzbuzz code
+
+# "Write a program that prints the numbers from 1 to 100.
+# iterate through the number 1-100 
+for x in range(101):
+    #print(x)
+    # For numbers which are multiples of both three and five print “FizzBuzz”.
+    if x%3 == 0 and x%5 == 0:
+        print("Fizzbuzz")
+    # But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+    elif x%3 == 0:
+        print("Fizz")
+    elif x%5 == 0:
+        print("Buzz")
+
+
+

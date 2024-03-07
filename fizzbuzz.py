30bb2fc670bbbaa713b422b726695eb1f57e56f8
ckirbyco <cek5dx@missouri.edu>
2024-03-06 21:33:44 -0600

    Committing changes made to step-three

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,22 @@
 # Fizzbuzz code
-# Branch step-two: Improving efficiency of the code
+# Branch step-three: Cleaning up the code
 
-# "Write a program that prints the numbers from 1 to 100.
-# iterate through the number 1-100
+# Iterating through the numbers 1-100
 for x in range(1, 101):
-    #print(x)
-    # For numbers which are multiples of both three and five print “FizzBuzz”.
+
+    # Multiples of both three and five print the word “FizzBuzz”.
     if x%3 == 0 and x%5 == 0:
         print("Fizzbuzz")
-    # But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+
+    # Multiples of three print “Fizz”.
     elif x%3 == 0:
         print("Fizz")
+
+    # Multiples of five print “Buzz”.
     elif x%5 == 0:
         print("Buzz")
-    # Adding in else for when the conditions above aren't met. 
+
+    # Else condition for when previous conditions aren't met.
     else:
         print(x)
 
-# Testing range
-for y in range (1,101):
-    print(y)
